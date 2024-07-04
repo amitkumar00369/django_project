af34eb6f9038d6a5491621b06e748e915dd47799
Amit Kumar <amitraazec53@gmail.com>
2024-07-04 11:30:41 +0530

    ch

--- p1/settings.py
+++ p1/settings.py
@@ -92,22 +92,22 @@ WSGI_APPLICATION = "p1.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Local_DB',
-        'USER': 'postgres',
-        'PASSWORD': 'Anshu',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Local_DB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Anshu',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
